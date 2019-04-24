180b234e1ecc58b39ac887cf39047dc285416f33
xjl;gvho;idfsjhlkfdsjhlk <thomas.colomba30@gmail.com>
2019-04-25 00:28:29 +0200

    change to red green and blue views

---
+++ src/views/MyBlueScreen.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="about">
+    <h1>This is my blue page</h1>
+  </div>
+</template>
+
+<script>
+module.exports = {
+	data : function(){
+		return {
+			greeting:'hello'
+		}
+	}
+}
+</script>
+
+<style scoped>
+.about {
+	color:blue;
+}
+</style>
